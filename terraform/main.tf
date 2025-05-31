043de5f6a407f14d3efb0a1703125eec24586c1a
Daniel Edwards <95729902+datboyblu3@users.noreply.github.com>
2025-05-31 15:27:54 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,29 @@
+locals {
+    network              = "vmbr0"
+    image                = "ubuntu-temp" 
+    ssh_user             = "worldboss"
+    private_key_path     = ""
+
+    target_servers = {
+      target-1 = {
+        machine_type = "ubuntu-server"
+        name         = "nginx"
+    }
+      target-2 = {
+        machine_type = "ubuntu-server"
+        name         = "wordpress"
+    }
+
+      target-3 = {
+        machine_type = "ubuntu-server"
+        name         = "smb"
+    }
+}
+
+
 resource "proxmox_vm_qemu" "vm-instance" {
+
+  for each = local.target_servers
     name                = "vm-instance"
     target_node         = "pve"
     clone               = "your-template-name"
@@ -19,5 +44,9 @@ resource "proxmox_vm_qemu" "vm-instance" {
         firewall  = false
         link_down = false
     }
+
+    provisioner "local-exec" {
+       command     = "ansible-playbook -i '${self.public_ip},'  -u opc --private-key '~/.ssh/oci_key' -e '~/.ssh/oci_key.pub' get_stager.yml -vvv --ask-become-pass"
+      }
 
 }

fff778ad7dd19c8c369002e110e09cdb2ef31f5b
Daniel Edwards <95729902+datboyblu3@users.noreply.github.com>
2025-05-31 15:50:23 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,45 +2,51 @@ locals {
     network              = "vmbr0"
     image                = "ubuntu-temp" 
     ssh_user             = "worldboss"
+    storage              = "local-lvm"
+    target_node          = "pve"
     private_key_path     = ""
 
     target_servers = {
       target-1 = {
-        machine_type = "ubuntu-server"
-        name         = "nginx"
+        machine_type        = "ubuntu-server"
+        target_name         = "nginx"
     }
       target-2 = {
-        machine_type = "ubuntu-server"
-        name         = "wordpress"
+        machine_type        = "ubuntu-server"
+        target_name         = "wordpress"
     }
 
       target-3 = {
-        machine_type = "ubuntu-server"
-        name         = "smb"
+        machine_type        = "ubuntu-server"
+        target_name         = "smb"
     }
+  }
 }
 
 
 resource "proxmox_vm_qemu" "vm-instance" {
 
-  for each = local.target_servers
-    name                = "vm-instance"
-    target_node         = "pve"
-    clone               = "your-template-name"
+  for_each = local.target_servers
+    target_server_id    = each.key
+    name                = each.value.target_name
+    machine_type        = each.value.machine_type
+
+    node                = local.target_node
+    clone               = local.image
     full_clone          = true
     cores               = 2
-    memory              = 2048
+    memory              = 4000
 
     disk {
         size            = "32G"
         type            = "scsi"
-        storage         = "your-storage-volume"
+        storage         = local.storage
         discard         = "on"
     }
 
     network {
         model     = "virtio"
-        bridge    = "vmbr0"
+        bridge    = local.network
         firewall  = false
         link_down = false
     }
@@ -49,4 +55,10 @@ resource "proxmox_vm_qemu" "vm-instance" {
        command     = "ansible-playbook -i '${self.public_ip},'"
       }
 
+}
+
+output "target_server_ips" {
+  value = {
+    for k, v in proxmox_vm_qemu : k => "http://${}" # Still working
+  }
 }

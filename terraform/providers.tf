a41c7ad81e19bb3028bffb4547a6ae8e3023a17a
Daniel Edwards <95729902+datboyblu3@users.noreply.github.com>
2025-05-31 10:01:47 -0400

    Update and rename provider.tf to providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc9"
+    }
+  }
+}
+
+provider "proxmox" {
+  # Configuration options
+
+    pm_api_url          = var.api_url
+    pm_api_token_id     = var.api_token_id
+    pm_api_token_secret = var.api_token_secret
+    pm_tls_insecure     = var.tls_insecure
+}

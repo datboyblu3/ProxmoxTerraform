e91bb07cce2baf42648565b39edc39641c591f3a
Daniel Edwards <95729902+datboyblu3@users.noreply.github.com>
2025-05-31 14:39:06 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,14 @@
+variable "api_url" {
+  type = string
+  description = API URL
+}
 
+variable "api_token_id" {
+  type = string
+  description = token id of the api
+}
+
+variable "api_token_secret" {
+  type = string
+  description = API token credential
+}
